6670a4fbad09da211f8e4fb8c4842dfa505ad778
austindickey <austindickey@me.com>
2020-04-11 13:15:01 -0500

    the petfinder api is now working and displaying to the adopt page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,27 @@
 var db = require("../models")
 var petfinder = require("@petfinder/petfinder-js")
-var client = new petfinder.Client({apiKey: "keb5oP5dpxdKjhpDbcve88lw5QP2uyFADw1xSrqZVTUFPItI1Y", secret: "CI9zv2Hejza5FGOi2xd6qvnH877dAayCMlEPGiy9"})
+var client = new petfinder.Client({apiKey: process.env.apiKey, secret: process.env.secret})
 
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    db.Dog.findAll({}).then(function (data) {
+    db.Dog.findAll().then(function (data) {
       res.render("index")
     })
   })
 
   // Load add page
   app.get("/add", function (req, res) {
-    db.Dog.findAll({
-      order: [["updatedAt", "DESC"]]
-    }).then(function (data) {
+    db.Dog.findAll().then(function (data) {
       res.render("add")
     })
   })
 
-  // Load adopt page
-  app.get("/adopt", function (req, res) {
-    res.render("adopt")
-  })
-
   // Load all dogs
   app.get('/dogs', (req, res) =>
-    db.Dog.findAll()
+    db.Dog.findAll({
+      order: [["id", "DESC"]]
+    })
       .then(dogs => res.render("dogs", { dogs }))
       .catch(err => console.log(err)))
 
@@ -67,25 +62,15 @@ module.exports = function (app) {
       .catch(err => console.log(err))
   })
 
-  app.get("/testAdopt", function (req, res) {
+  // Api call and adopt page route
+  app.get("/adopt", function (req, res) {
 
     client.animal.search({type: "Dog"})
     .then(function (response) {
         var data = response.data.animals
-       // res.json(data[0])
-        
-      //   var available = $("#availablePets")
-      //   var newH3 = $("<h3>")
-      //   newH3.text(data[0].name)
-      //   available.append(newH3)
-      //   console.log("JASON================")
-      //  console.log(available)
-        res.render("adopt", {data} );
-  //  res.json(available)
+        res.render("adopt", {data} )
     })
-    .catch(function (error) {
-        // Handle the error
-    })
+    .catch(err => console.log(err))
 })
 
   // Render 404 page for any unmatched routes
